f04f6ad2c34142290a9e3046451ff5c2c6c2ac2b
Stephen <sblackstone@gmail.com>
2019-01-16 22:18:33 -0500

    Game components

--- deep_yahtzee/dice/dice.py
+++ deep_yahtzee/dice/dice.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+NUM_DICE = 5
 ROLLS_PER_TURN=3
 
 class Dice:
@@ -7,9 +8,54 @@ class Dice:
     def __init__(self):
         self.reset()
 
+    def set_die_count(self):
+        self.die_count = [0] * 7
+        for i in self.dice:
+            self.die_count[i] += 1 
+        return(self.die_count)
+
     def reset(self):
-        self.dice  = [0, 0, 0, 0, 0]
+        self.die_count = [0] * 6
+        self.dice  = [0] * 5
         self.rolls = ROLLS_PER_TURN
+        
+    def classifications(self):
+        self.classifications = { "chance": sum(self.dice) }
+        
+        for i in range(1,7):
+            if self.die_count[i] > 0:
+                key = "main_{}".format(i)
+                self.classifications[key] = self.die_count[i] * i
+
+            if self.die_count[i] >= 3:
+                key = "three_of_a_kind"
+                self.classifications[key] = sum(self.dice)
+                for j in range(1,7):
+                    if i != j and self.die_count[j] == 2:
+                        key = "full_house"
+                        self.classifications[key] = 25                        
+
+            if self.die_count[i] >= 4:
+                key = "four_of_a_kind"
+                self.classifications[key] = sum(self.dice)
+
+            if self.die_count[i] == 5:
+                key = "yahtzee"
+                self.classifications[key] = 50
+
+        for i in range(1, 4):
+            if self.die_count[i] and self.die_count[i+1] and self.die_count[i+2] and self.die_count[i+3]:
+                key   = "small_straight"
+                self.classifications[key] = 30
+                if i < 3 and self.die_count[i+4]:
+                    key   = "large_straight"
+                    self.classifications[key] = 40
+                
+
+
+        return self.classifications
+        
+        
     
     # Accepts an array of the form [0,1,1,0,0]
     # which means re-roll dies 1 and 2.
@@ -17,14 +63,12 @@ class Dice:
     def roll(self, arr):
         if self.rolls == 0:
             return False
-
-        for i in range(5):
-
+        for i in range(NUM_DICE):
             # Ignore arr[i] if this is the first roll.
             if arr[i] or self.rolls == ROLLS_PER_TURN:
               self.dice[i] = np.random.randint(6) + 1
-
         self.rolls -= 1
+        self.set_die_count()
         return True
 
 
@@ -39,5 +83,6 @@ if __name__ == "__main__":
     print(d.dice)
     print(d.roll([1,0,1,0,1]))
     print(d.dice)
-
+    print(d.die_count[1:6])
+    print(d.classifications())
         
--- deep_yahtzee/scorepad/scorepad.py
+++ deep_yahtzee/scorepad/scorepad.py
@@ -1,3 +1,25 @@
+
+SCORE_TYPES = ['main_1', 'main_2', 'main_3', 'main_4', 'main_5', 'main_6', 'three_of_a_kind', 'four_of_a_kind', 'full_house', 'small_straight', 'large_straight', 'chance', 'yahtzee' ]
+
 class ScorePad:
     def __init__(self):
-        print("Init ScorePad")
+        self.reset()
+
+    def reset(self):
+        self.scores = {}
+    
+    def unscored_types(self):
+        return [x for x in SCORE_TYPES if not x in self.scores.keys()]
+    
+    def take_score(self, type, value):
+        if not type in self.unscored_types():
+            raise Exception('Score Already Taken') 
+        self.scores[type] = value
+        
+if __name__ == "__main__":
+    s = ScorePad()
+    print(s.unscored_types())
+    s.take_score('main_6', 24)
+    s.take_score('full_house', 25)
+    print(s.unscored_types())
+    print(s.scores)
